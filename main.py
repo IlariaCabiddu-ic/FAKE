316a5c5b3cec666b5a99afa41d8781c11db5f7cb
IlariaCabiddu-ic <62742408+IlariaCabiddu-ic@users.noreply.github.com>
2022-01-04 12:57:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''--------------Main file--------------'''
 import pandas as pd
 
 df_new = pd.read_json('path_of_the_file.json') # if we have a online repository, we can use the URL, df= pd.read_json(URL)
@@ -17,4 +18,27 @@ print("The source to be analysed is " + s)
 sub_df= df[df["Source"] == s]
 # detail sub_df
 sub_df.info()
+
+# STEP TWO check Top Level Domain (TLD)
+i=len(s)-1
+TLD=''
+while s[i]!='.':
+    TLD = TLD + s[i]
+    i=i-1
+TLD = TLD[::-1] # TLD extraction of the source to be analysed
+#print(TLD)
+
+TLDs=['gov','edu','mil','museum','jobs','post', 'travel']
+
+if TLD in TLDs:
+    print("This source is trusted!!!\n")
+else:
+    print("This source needs to be analysed\n")
+    # STEP THREE Detectio of all topic
+
+
+
+    # we have to compute model trust
+
+
 
