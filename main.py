560459c4690b2082deb63434594e100e34b1a4c8
IlariaCabiddu-ic <62742408+IlariaCabiddu-ic@users.noreply.github.com>
2022-01-04 18:20:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-'''--------------Main file--------------'''
+'''--------------Main file-------------- '''
 import pandas as pd
+import model_trust
 
 df_new = pd.read_json('path_of_the_file.json') # if we have a online repository, we can use the URL, df= pd.read_json(URL)
 # visualization of info dataframe (optional)
@@ -15,30 +16,26 @@ s = input()
 print("The source to be analysed is " + s)
 
 # create a df with only data of source s
-sub_df= df[df["Source"] == s]
+sub_df = df[df["Source"] == s]
 # detail sub_df
 sub_df.info()
 
 # STEP TWO check Top Level Domain (TLD)
-i=len(s)-1
-TLD=''
-while s[i]!='.':
+i = len(s)-1
+TLD = ''
+while s[i] != '.':
     TLD = TLD + s[i]
-    i=i-1
-TLD = TLD[::-1] # TLD extraction of the source to be analysed
-#print(TLD)
+    i = i-1
+TLD = TLD[::-1]  # TLD extraction of the source to be analysed
+# print(TLD)
 
-TLDs=['gov','edu','mil','museum','jobs','post', 'travel']
+TLDs = ['gov', 'edu', 'mil', 'museum', 'jobs', 'post', 'travel']
 
 if TLD in TLDs:
     print("This source is trusted!!!\n")
 else:
     print("This source needs to be analysed\n")
-    # STEP THREE Detectio of all topic
+    # STEP THREE Detection of all topic
+    topics = sub_df['Topic'].unique()
 
-
-
-    # we have to compute model trust
-
-
-
+    """--------------- MODEL TRUST ---------------------"""
